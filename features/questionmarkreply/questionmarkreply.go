1ea57b590db919b4c3e5f25f09a5e6bbf6435071
秋のかえで <autmaple@protonmail.com>
2022-07-22 00:53:48 +0000

    feat: question mark pyramid

--- features/questionmarkreply/questionmarkreply.go
+++ features/questionmarkreply/questionmarkreply.go
@@ -1,6 +1,8 @@
 package questionmarkreply
 
 import (
+	"strings"
+
 	"github.com/AkinoKaede/question-mark-reply-bot/common"
 	"github.com/AkinoKaede/question-mark-reply-bot/features"
 	tele "gopkg.in/telebot.v3"
@@ -17,6 +19,26 @@ func OnText(c tele.Context) error {
 	for _, b := range text {
 		if !common.Contains(string(b), QuestionMarks) {
 			return nil
+		}
+	}
+
+	if c.Message().ReplyTo != nil && len(text) > 1 {
+		markCount := make(map[rune]int)
+		for _, b := range text {
+			markCount[b]++
+		}
+
+		for k, v := range markCount {
+			if v == len(text) {
+				replyToText := c.Message().ReplyTo.Text
+				if strings.Count(replyToText, string(k)) == len(replyToText) {
+					replyMsg := &strings.Builder{}
+					replyMsg.WriteString(text)
+					replyMsg.WriteRune(k)
+
+					return c.Reply(replyMsg.String())
+				}
+			}
 		}
 	}
 
