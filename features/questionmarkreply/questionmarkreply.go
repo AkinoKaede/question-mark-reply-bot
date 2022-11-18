1d56346f92851a2adb6cfba8b7936e493932f7b9
秋のかえで <autmaple@protonmail.com>
2022-11-18 19:09:59 +0800

    feat: always reply sicker in private chat

--- features/questionmarkreply/questionmarkreply.go
+++ features/questionmarkreply/questionmarkreply.go
@@ -73,5 +73,5 @@ func OnSticker(c tele.Context) error {
 
 func init() {
 	features.RegisterFeature(tele.OnText, OnText)
-	features.RegisterFeature(tele.OnSticker, OnSticker, Random(10))
+	features.RegisterFeature(tele.OnSticker, OnSticker, RandomForNotPrivate(10))
 }
--- features/questionmarkreply/random.go
+++ features/questionmarkreply/random.go
@@ -7,9 +7,13 @@ import (
 	tele "gopkg.in/telebot.v3"
 )
 
-func Random(probability int) tele.MiddlewareFunc {
+func RandomForNotPrivate(probability int) tele.MiddlewareFunc {
 	return func(next tele.HandlerFunc) tele.HandlerFunc {
 		return func(c tele.Context) error {
+			if c.Chat().Type == tele.ChatPrivate {
+				return next(c)
+			}
+
 			rand.Seed(time.Now().UnixNano())
 			randNum := rand.Intn(100)
 
