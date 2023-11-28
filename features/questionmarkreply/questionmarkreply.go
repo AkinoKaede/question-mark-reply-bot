87e66067d084f5f18a3468ab3a9b242f2649cc6d
KallyDev <kallydev@gmail.com>
2023-11-28 15:38:01 +0800

    feat: support more question mark characters

--- features/questionmarkreply/questionmarkreply.go
+++ features/questionmarkreply/questionmarkreply.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	QuestionMarks      = append([]rune{'?', '¿', '？'}, QuestionMarkEmojis...)
+	QuestionMarks      = append([]rune{'?', '¿', '⁇', '︖', '﹖', '？', '？'}, QuestionMarkEmojis...)
 	QuestionMarkEmojis = []rune{'❓', '❔'}
 )
 
